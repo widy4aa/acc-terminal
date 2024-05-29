3008f389227f84ee84e3f6db349ec69f194b3d2f
widy4aa <dio34678@gmail.com>
2024-05-29 09:49:44 +0700

    add pengguna features

--- pengguna_controller.py
+++ pengguna_controller.py
@@ -4,6 +4,16 @@ import helpers as helper
 conn = psycopg2.connect(database='Acc', user='postgres', password='dio', host='localhost', port=5432)
 cur = conn.cursor()
 
+def get_pengguna (cur,id_pengguna):
+    query = f"""
+    select username,nama_pengguna,password
+    from pengguna
+    where id_pengguna = {id_pengguna}
+    """
+    cur.execute(query)
+    data = cur.fetchall()
+    return data
+
 def list_all_users(cur) :
     query = f"""
         select id_pengguna,username,password,nama_role from pengguna p join role r
@@ -22,4 +32,23 @@ def delete_pengguna(cur,id_pengguna):
     cur.execute(query)
     conn.commit()
 
+def edit_pengguna(cur,id_pengguna,data_baru):
+    #helper.dd(data_baru)
+    query = f"""
+        update pengguna
+        set username = '{data_baru[0]}',
+        nama_pengguna = '{data_baru[1]}',
+        password ='{data_baru[2]}'
+        where id_pengguna = {id_pengguna}
+    """
+    cur.execute(query)
+    conn.commit()
 
+def tambah_pengguna(cur,data_pengguna):
+    query = f"""
+        insert into pengguna 
+        (username,nama_pengguna,password,id_role)
+        values ('{data_pengguna[0]}','{data_pengguna[1]}','{data_pengguna[2]}',2)
+    """
+    cur.execute(query)
+    conn.commit()
--- pengguna_view.py
+++ pengguna_view.py
@@ -4,7 +4,6 @@ import helpers as helper
 from tabulate import tabulate
 
 
-dump = [('Almashuda34', 'Huda', 'huda34678', 'owner')]
 
 def pengguna(status_login):
     print(tabulate(controller.list_all_users(controller.cur),headers=['id_pengguna','username','nama','password','status']))     
@@ -18,16 +17,30 @@ def pengguna(status_login):
     if menu == 1 :
         id_pengguna = input('masukkan id pengguna untuk pengguna yang ingin dihapus : ')
         controller.delete_pengguna(controller.cur,id_pengguna)
+        helper.clear()
         pengguna(status_login)
         
     elif menu ==  2  :
-        print ("edit pengguna")
+        id_pengguna = input("pilih  pengguna yang ingin di edit : ")
+        data_lama = controller.get_pengguna(controller.cur,id_pengguna)
+        username = input((data_lama[0][0] + ' :')) or data_lama[0][0]
+        nama_pengguna = input((data_lama[0][1] + ' :')) or data_lama[0][1]
+        password = input((data_lama[0][2] + ' :')) or data_lama[0][2]
+        data_baru = [username,nama_pengguna,password]
+        controller.edit_pengguna(controller.cur,id_pengguna,data_baru)
+        
+        
     elif menu == 3 :
-        print ("tambah ppengguna")
+        username = input("masukkan username : ")
+        nama = input ("masukkan nama : ")
+        password = input("masukkan password : ")
+        data_baru = [username,nama,password]
+        controller.tambah_pengguna(controller.cur,data_baru)
+        helper.clear()
+        pengguna(status_login)   
     elif menu == 4 :
         helper.clear()
         dashboard.dashboard(status_login)
     else :
         pengguna(status_login)
         
-pengguna(dump)
